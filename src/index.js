2531e099a42eaa81bf38622546c120142d06ad37
przemwo <przemwo@o2.pl>
2016-12-15 18:46:08 +0100

    stacks with private items - WeakMaps

--- src/index.js
+++ src/index.js
@@ -1,34 +1,61 @@
-function Stack() {
-  let items = [];
-  this.push = function(element) {
-    items.push(element);
-  };
-  this.pop = function() {
-    return items.pop();
-  };
-  this.peek = function() {
-    return items[items.length - 1];
-  };
-  this.isEmpty = function() {
-    return items.length === 0;
-  };
-  this.size = function() {
-    return items.length;
-  };
-  this.clear = function() {
-    items = [];
-  };
-  this.print = function() {
-    console.log(JSON.stringify(items, null, 2));
+let Stack = (function() {
+  const items = new WeakMap();
+  class Stack {
+    constructor() {
+      items.set(this, []);
+    }
+    push(element) {
+      let s = items.get(this);
+      s.push(element);
+    };
+    pop () {
+      let s = items.get(this);
+      let r = s.pop();
+      return r;
+    };
+    peek () {
+      let s = items.get(this);
+      return s[s.length - 1];
+    };
+    isEmpty () {
+      let s = items.get(this);
+      return s.length === 0;
+    };
+    size () {
+      let s = items.get(this);
+      return s.length;
+    };
+    clear () {
+      items.set(this, []);
+    };
+    print () {
+      console.log(JSON.stringify(items.get(this), null, 2));
+    }
   }
-}
+  return Stack;
+})();
 
-let stack = new Stack;
+let stack = new Stack();
+console.log("INIT ==============");
+stack.print();
 console.log(stack.isEmpty());
-stack.push('a');
+
+console.log("PUSH ==============");
+stack.push(2);
+stack.push(3);
+stack.push(5);
 stack.print();
-stack.push('b');
-stack.print();
+
+console.log("POP ==============");
 stack.pop();
+stack.print();
+
+console.log("PEEK ==============");
+console.log(stack.peek());
+console.log(stack.isEmpty());
+console.log(stack.size());
+
+console.log("CLEAR ==============");
+stack.clear();
 stack.print();
 console.log(stack.isEmpty());
